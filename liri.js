64e98807d927f0c7ee36769eb1f2c82dca3416f7
Hector M <moralh2@gmail.com>
2018-12-16 09:54:07 -0600

    update console log for spotify

--- liri.js
+++ liri.js
@@ -1,31 +1,36 @@
 require("dotenv").config();
-
 var keys = require("./keys.js")
+
+var inputs = process.argv
+
 var Spotify = require('node-spotify-api');
 
 
 var spotify = new Spotify(keys.spotify);
+
+console.log(inputs)
+
+
 
 
 spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
-    console.log(data.tracks.items[0].name); 
-    console.log(data.tracks.items[0].track_number); 
-    console.log(data.tracks.items[0].album.name); 
-    console.log(data.tracks.items[0].album.release_date); 
-    console.log(data.tracks.items[0].artists[0].name); 
 
+    var artist = data.tracks.items[0].artists[0].name
+    var song = data.tracks.items[0].name
+    var track_info = "(Track " + data.tracks.items[0].track_number + " of " + data.tracks.items[0].album.total_tracks + ")"
+    var album = data.tracks.items[0].album.name + " (Released: " + data.tracks.items[0].album.release_date + ")"
+    var url = data.tracks.items[0].href
+    
 
-
-
-//   console.log(data.tracks.items[0].name); 
-//   console.log(data.tracks.items[0].album); 
-//   console.log(data.tracks.items[0].track_number); 
-//   console.log(data.tracks.items[0]).artists.name; 
-//   console.log(data.tracks.items[0]); 
-
+    console.log(
+        "Artist: " + artist + "\n" + 
+        "Album: " + album+ "\n" +
+        "Song: " + song + " " + track_info + "\n" + 
+        "URL: " + url
+    )
 
 });
 
